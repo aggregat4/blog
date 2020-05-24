cfe97016e7c46f85eee651b46e60e3fbc45c4521
Boris Terzic <boris@terzic.be>
2020-05-24 10:33:12 +0200

    Adding back the eleventy configuration file Changing eleventy configuration to publish to a "docs" subdirectory so we can more easily publish with github-pages

---
+++ .eleventy.js
@@ -0,0 +1,24 @@
+const toml = require("toml");
+
+/*
+This configuration file is picked up by filename convention.
+*/
+module.exports = function(eleventyConfig) {
+  // or, use a Universal filter (an alias for all of the above)
+  eleventyConfig.addFilter("isodate", function(date) {
+    return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")}`
+  })
+
+  eleventyConfig.setFrontMatterParsingOptions({
+    engines: {
+      toml: toml.parse.bind(toml)
+    }
+  })
+
+  return {
+    dir: {
+      output: "docs"
+    }
+  }
+
+}
